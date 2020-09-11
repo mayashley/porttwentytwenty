025ee33d7006631f8b3d956e95b9e6e045e32600
Ashley <mayashley052@gmail.com>
2020-09-10 21:03:28 -0400

    worker on hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,41 +4,48 @@ import styled from "styled-components";
 const Hero = () => {
   return (
     <Wrapper>
-        <div className="heroWrapper">
-            <div className="secondaryWrapper">
-      <h1>
-        Hi, I'm Ashley, <strong>Web Designer</strong> and
-        <strong>Front-End Developer</strong>.
-      </h1>
-      <h3>
-        I design and build beautiful websites for businesses in the Southern
-        Ohio area. If you need a modern and powerful website, send me an email.
-        If we are a good fit, I will give you a time and cost estimate.
-      </h3>
-      </div>
+      <div className="heroWrapper">
+        <div className="secondaryWrapper">
+          <h1>
+            Hi, I'm Ashley, <strong>Web Designer</strong> and
+            <strong>Front-End Developer</strong>.
+          </h1>
+          <h3>
+            I design and build beautiful websites for businesses. If you need a modern and powerful website, send me an
+            email. If we are a good fit, I will give you a time and cost
+            estimate.
+          </h3>
+          <button
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = "/";
+              // need to add the location for the link
+            }}> see my work</button>
+        </div>
       </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.section`
-.heroWrapper{
-    border:1px solid blue;
+  .heroWrapper {
+    border: 1px solid blue;
     background-color: var(--almostBlack);
     width: 100%;
     height: 100%;
     max-height: 1024px;
     min-height: 640px;
-    display:flex;
-}
-.secondaryWrapper{
-    display:flex;
-    flex-direction:column;
-  justify-content: center;
- margin: 0 0 0 50px;
-}
+    display: flex;
+  }
+  .secondaryWrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 0 0 50px;
+  }
   h1 {
-     color: var(--white);
+    color: var(--white);
     font-family: Roboto;
     font-weight: 200;
     font-size: 66px;
@@ -48,16 +55,25 @@ const Wrapper = styled.section`
   }
   h3 {
     color: var(--white);
-      font-family: Roboto;
-      font-weight:300;
-      font-size: 20px;
-      max-width: 700px;
+    font-family: Roboto;
+    font-weight: 300;
+    font-size: 20px;
+    max-width: 700px;
+  }
+  button{
+    width:120px;
+    height: 34px;
+    margin: 20px 0;
+    color: var(--almostBlack);
+    background-image: linear-gradient(to right, rgba(143, 215, 87, 1), rgba(58, 128, 4, 1));
+    border-radius:4px;
+    border: none;
+
   }
 
-
   //   @media (max-width: 768px) {
-    //     flex-direction: column;
-    //   }
+  //     flex-direction: column;
+  //   }
 `;
 
 export default Hero;

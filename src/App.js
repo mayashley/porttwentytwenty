c1b37a02f7ae463aede411d99f57f634df04a81f
Ashley <mayashley052@gmail.com>
2020-09-15 12:10:07 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Hero from "./components/Hero";
-import TopMenu from "./components/TopMenu";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <TopMenu/>
+    
      <Hero/>
     </div>
   );
